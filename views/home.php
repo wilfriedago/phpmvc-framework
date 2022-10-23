3d3a629e8e2ff007d67c4f6c3c406c39aa1b2ad1
Wilfried AGO <wilfriedago@pm.me>
2022-10-23 10:53:59 +0100

    Updated Home View to test POST Request ! & manipulate data in Controller

--- views/home.php
+++ views/home.php
@@ -1,3 +1,12 @@
 <h1 class="text-5xl text-white font-bold">
     Welcome to the Home <?= $name ?>
+    <form class="form" method="post" action="/contact">
+        <label class="" for="name">
+            Name
+            <input class="form-input text-slate-900" id="name" type="text" name="name" >
+        </label>
+        <button class="text-xl" type="submit">
+            Send
+        </button>
+    </form>
 </h1>
