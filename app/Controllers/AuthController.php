44daff307fd997c6ce39269eaa3710af686a95aa
Wilfried AGO <wilfriedago@pm.me>
2022-10-31 11:41:21 +0100

    Implementing Validator class & User validation rules in AuthController

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,21 +4,50 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Core\Request;
+use App\Core\Response;
+use App\Core\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(Request $request): string
+    /**
+     * @param Request $request
+     * @return Response|string
+     */
+    public function login(Request $request): Response | string
     {
         if ($request->isPost()) {
-            return "Submitted data";
+            return (new Response())->json(["success" => "Resource created !"])->setStatus(201);
         }
         return $this->setLayout('auth')->render('login');
     }
 
-    public function register(Request $request):string
+    /**
+     * @param Request $request
+     * @return Response|string
+     */
+    public function register(Request $request): Response | string
     {
         if ($request->isPost()) {
-            return "Submitted data";
+            $user = new User($request);
+
+            $validator = new Validator($user);
+            $validator->rules([
+                'firstname' => [Validator::RULE_REQUIRED],
+                'lastname' => [Validator::RULE_REQUIRED],
+                'username' => [Validator::RULE_REQUIRED],
+                'email' => [Validator::RULE_REQUIRED, Validator::RULE_EMAIL],
+                'password' => [Validator::RULE_REQUIRED, Validator::RULE_MIN => 8, Validator::RULE_MAX => 24],
+                'passwordConfirm' => [Validator::RULE_REQUIRED, Validator::RULE_MATCH => 'password']
+            ]);
+
+
+
+            if (!$validator->validate()) {
+                return (new Response())->json($validator->errors)->setStatus(403);
+            }
+
+            // return (new Response())->json(["success" => "Resource created !"])->setStatus(201);
         }
         return $this->setLayout('auth')->render('register');
     }
---
+++ app/Core/Validator.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Core;
+
+class Validator
+{
+    const RULE_REQUIRED = 'required';
+    const RULE_EMAIL = 'email';
+    const RULE_MIN = 'min';
+    const RULE_MAX = 'max';
+    const RULE_MATCH = 'match';
+
+    public array $data = [];
+    public array $rules = [];
+    public array $errors = [];
+
+    private Model $model;
+
+    /**
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param array $rules
+     * @return void
+     */
+    public function rules(array $rules): void
+    {
+        $this->rules = $rules;
+    }
+
+    /**
+     * @return bool
+     */
+    public function validate(): bool
+    {
+        foreach ($this->rules as $attribute => $attributeRules) {
+            $value = $this->model->{$attribute};
+
+            foreach ($attributeRules as $rule => $ruleValue) {
+                if (!is_string($rule)) {
+                    $rule = $ruleValue;
+                }
+
+                if ($rule === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, "This field is required");
+                }
+
+                if ($rule === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, "This field must be a valid email address");
+                }
+
+                if ($rule === self::RULE_MIN && strlen($value) < $ruleValue) {
+                    $this->addError($attribute, "This field must be a minimum of $ruleValue characters");
+                }
+
+                if ($rule === self::RULE_MAX && strlen($value) > $ruleValue) {
+                    $this->addError($attribute, "This field must be a maximum of $ruleValue characters");
+                }
+
+                if ($rule === self::RULE_MATCH && $value !== $this->model->{$ruleValue}) {
+                    $this->addError($attribute, "This field must be the same as $ruleValue");
+                }
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    /**
+     * @param $attribute
+     * @param $message
+     * @return void
+     */
+    public function addError($attribute, $message): void
+    {
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError()
+    {
+        return $this->errors[array_key_first($this->errors)] ?? false;
+    }
+}
